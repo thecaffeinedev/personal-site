79ac73d2fac726b8150b9631abc4d19b263a0b37
Prabhat <iprabhatdev@gmail.com>
2023-10-29 20:26:20 +0530

    chore: site metadata edit

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,31 +1,32 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
+  title: 'Personal Blog ',
+  author: 'Prabhat',
   headerTitle: 'TailwindBlog',
-  description: 'A blog created with Next.js and Tailwind.css',
+  description: 'I am a software engineer 🧑🏻‍💻 based in Hyderabad, India, who enjoys building things.',
   language: 'en-us',
   theme: 'system', // system, dark or light
-  siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
-  siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
+  siteUrl: process.env.NODE_ENV === 'production' ? 'https://iprabhat.dev' : 'http://localhost:3001',
+  siteRepo: 'https://github.com/TheCaffeineDev/personal-site',
   siteLogo: '/static/images/logo.png',
   socialBanner: '/static/images/twitter-card.png',
-  mastodon: 'https://mastodon.social/@mastodonuser',
-  email: 'address@yoursite.com',
-  github: 'https://github.com',
-  twitter: 'https://twitter.com/Twitter',
-  facebook: 'https://facebook.com',
-  youtube: 'https://youtube.com',
-  linkedin: 'https://www.linkedin.com',
+  // mastodon: 'https://mastodon.social/@mastodonuser',
+  email: 'iprabhatdev@gmail.com',
+  github: 'https://github.com/thecaffeinedev',
+  twitter: 'https://twitter.com/thecaffeinedev',
+  instagram: 'https://www.instagram.com/caffeinedev/',
+  youtube: 'https://www.youtube.com/channel/UC-756oKXdvH8GZEDF7GHIlw',
+  stackoverflow: 'https://stackoverflow.com/users/8105979/prabhat-kumar-sahu',
+  linkedin: 'https://www.linkedin.com/in/prabhat-kumar-sahu-b9a53674/',
   locale: 'en-US',
   analytics: {
     // If you want to use an analytics provider you have to add it to the
     // content security policy in the `next.config.js` file.
     // supports Plausible, Simple Analytics, Umami, Posthog or Google Analytics.
-    umamiAnalytics: {
-      // We use an env variable for this site to avoid other users cloning our analytics ID
-      umamiWebsiteId: process.env.NEXT_UMAMI_ID, // e.g. 123e4567-e89b-12d3-a456-426614174000
-    },
+    // umamiAnalytics: {
+    //   // We use an env variable for this site to avoid other users cloning our analytics ID
+    //   umamiWebsiteId: process.env.NEXT_UMAMI_ID, // e.g. 123e4567-e89b-12d3-a456-426614174000
+    // },
     // plausibleAnalytics: {
     //   plausibleDataDomain: '', // e.g. tailwind-nextjs-starter-blog.vercel.app
     // },
@@ -33,9 +34,9 @@ const siteMetadata = {
     // posthogAnalytics: {
     //   posthogProjectApiKey: '', // e.g. 123e4567-e89b-12d3-a456-426614174000
     // },
-    // googleAnalytics: {
-    //   googleAnalyticsId: '', // e.g. G-XXXXXXX
-    // },
+    googleAnalytics: {
+      googleAnalyticsId: 'UA-150491781-1', // e.g. G-XXXXXXX
+    },
   },
   newsletter: {
     // supports mailchimp, buttondown, convertkit, klaviyo, revue, emailoctopus
