df40a203785faed59fcf231caf2b9366d38391e7
Prabhat Kumar Sahu <prabhat1015@gmail.com>
2025-01-20 12:55:18 +0530

    Google Analytics ID change

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -35,7 +35,7 @@ const siteMetadata = {
     //   posthogProjectApiKey: '', // e.g. 123e4567-e89b-12d3-a456-426614174000
     // },
     googleAnalytics: {
-      googleAnalyticsId: 'UA-150491781-1', // e.g. G-XXXXXXX
+      googleAnalyticsId: 'G-QCX4GXLLQX', // e.g. G-XXXXXXX
     },
   },
   newsletter: {
